48ed1b096d96f305750ccef817e1beab2398d850
Jamie Winsor <jamie@vialstudios.com>
2012-03-06 17:26:15 -0800

    explicitly specify name as "artifact"

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name             "artifact"
 maintainer       "Riot Games"
 maintainer_email "jamie@vialstudios.com"
 license          "All rights reserved"
 description      "Provides your cookbooks with the Artifact Deploy LWRP"
-version          "0.9.0"
+version          "0.9.1"
 
 %w{ centos redhat fedora }.each do |os|
   supports os
