951bf1f9e5eb254d2aff519eefd4412ffe24202d
jhowarth <jhowarth@riotgames.com>
2012-06-14 14:28:28 -0700

    Add ability to retrieve nexus artifacts.

---
+++ libraries/chef_artifact.rb
@@ -0,0 +1,22 @@
+class Chef
+  module Artifact
+    DATA_BAG = "artifact"
+
+    class << self
+      def nexus_config_for(node)
+        data_bag_key = "nexus"
+        begin
+          data_bag_item = Chef::EncryptedDataBagItem.load(DATA_BAG, data_bag_key)
+        rescue Net::HTTPServerException => e
+          raise EncryptedDataBagNotFound.new(data_bag_key)
+        end
+
+        config = data_bag_item[node.chef_environment]
+        unless config
+          raise EnvironmentNotFound.new(data_bag_key, node.chef_environment)
+        end
+        config
+      end
+    end
+  end
+end
