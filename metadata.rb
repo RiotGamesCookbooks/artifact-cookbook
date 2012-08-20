3aac06e245d44d9805aa8b3c2e0dd44e037142da
jhowarth <jhowarth@riotgames.com>
2012-08-20 11:13:25 -0700

    bump 0.10.3

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Riot Games"
 maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "Provides your cookbooks with the Artifact Deploy LWRP"
-version          "0.10.2"
+version          "0.10.3"
 
 %w{ centos redhat fedora }.each do |os|
   supports os
