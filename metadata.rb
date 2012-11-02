0e0420a50b9280eb3999f7906c1165f8c9e081d7
Jamie Winsor <jamie@vialstudios.com>
2012-11-01 17:24:43 -0700

    verison bump 0.11.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Riot Games"
 maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "Provides your cookbooks with the Artifact Deploy LWRP"
-version          "0.10.10"
+version          "0.11.0"
 
 %w{ centos redhat fedora }.each do |os|
   supports os
