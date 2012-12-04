45afc46e951c3488fdb9233fc6f624dce5775194
Jamie Winsor <jamie@vialstudios.com>
2012-12-04 15:57:01 -0800

    load non-encrypted data bags for nexus config if chef solo

--- libraries/chef_artifact.rb
+++ libraries/chef_artifact.rb
@@ -1,19 +1,32 @@
 class Chef
   module Artifact
-    DATA_BAG = "artifact"
+    DATA_BAG = "artifact".freeze
+    NEXUS_DBI = "nexus".freeze
 
     class << self
-      def nexus_config_for(node)
-        data_bag_key = "nexus"
-        begin
-          data_bag_item = Chef::EncryptedDataBagItem.load(DATA_BAG, data_bag_key)
-        rescue Net::HTTPServerException => e
-          raise EncryptedDataBagNotFound.new(data_bag_key)
+      # Return the nexus data bag item. An encrypted data bag item will be used if we are
+      # running as Chef Client and a standard data bag item will be used if running as
+      # Chef Solo
+      #
+      # @param [Chef::Node] node
+      #
+      # @return [Chef::DataBagItem, Chef::EncryptedDataBagItem]
+      def load_nexus_dbi(node)
+        if node[:chef_solo]
+          Chef::EncryptedDataBagItem.load(DATA_BAG, NEXUS_DBI)
+        else
+          Chef::DataBagItem.load(DATA_BAG, NEXUS_DBI)
         end
+      rescue Net::HTTPServerException
+        raise EncryptedDataBagNotFound.new(NEXUS_DBI)
+      end
+
+      def nexus_config_for(node)
+        data_bag_item = load_nexus_dbi(node)
 
         config = data_bag_item[node.chef_environment] || data_bag_item["*"]
         unless config
-          raise EnvironmentNotFound.new(data_bag_key, node.chef_environment)
+          raise EnvironmentNotFound.new(NEXUS_DBI, node.chef_environment)
         end
         config
       end
@@ -22,7 +35,7 @@ class Chef
         if version.casecmp("latest") == 0
           require 'nexus_cli'
           config = nexus_config_for(node)
-          remote = NexusCli::RemoteFactory.create(config)
+          remote = NexusCli::RemoteFactory.create(config, false)
           Nokogiri::XML(remote.get_artifact_info(artifact_location)).xpath("//version").first.content()
         else
           version
