d0acee7f2bc9aa403eeff076315d5f5f8329d16a
Michael Ivey <michael.ivey@riotgames.com>
2012-12-13 12:57:19 -0800

    Documentation

--- fixtures/mkartifact.sh
+++ fixtures/mkartifact.sh
@@ -1,4 +1,5 @@
 VERSION=$1
 : ${VERSION:="1.2.3"}
 tar cvfz artifact_test-$VERSION.tgz artifact_test_app
-nexus-cli push_artifact artifact_cookbook:test:$VERSION:tgz artifact_test-$VERSION.tgz
+
+# nexus-cli push_artifact artifact_cookbook:test:$VERSION:tgz artifact_test-$VERSION.tgz
