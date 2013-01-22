f01e9e0030788d1e3068b84e427506e4f56c52b1
Kyle Allan <kallan@riotgames.com>
2013-01-22 12:57:17 -0800

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Riot Games"
 maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Provides your cookbooks with the Artifact Deploy LWRP"
-version          "1.1.1"
+version          "1.1.2"
 
 %w{ centos redhat fedora }.each do |os|
   supports os
