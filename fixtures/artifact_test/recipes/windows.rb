a959173f480ddce7319f820eb2f0aecae7d31e7e
KAllan357 <KAllan357@gmail.com>
2013-02-27 12:59:39 -0800

    use file.join to avoid mishaps

--- fixtures/artifact_test/recipes/windows.rb
+++ fixtures/artifact_test/recipes/windows.rb
@@ -6,6 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+user "artifact"
+
 artifact_deploy "artifact_test" do
   version node[:artifact_test][:version]
   artifact_location node[:artifact_test][:location]
--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          "Apache 2.0"
 description      "Provides your cookbooks with the Artifact Deploy LWRP"
 version          "1.3.1"
 
-%w{ centos redhat fedora }.each do |os|
+%w{ centos redhat fedora windows }.each do |os|
   supports os
 end
 
