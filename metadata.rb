864b5db7b05e87ca4d9161ac628788d2f9837ebf
KAllan357 <KAllan357@gmail.com>
2013-03-20 14:59:03 -0700

    1.5.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Riot Games"
 maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Provides your cookbooks with the Artifact Deploy LWRP"
-version          "1.4.0"
+version          "1.5.0"
 
 supports "centos"
 supports "redhat"
