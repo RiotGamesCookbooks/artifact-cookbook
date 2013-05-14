9a8f024005d53b306433cc0627e9c66faec850ae
KAllan357 <KAllan357@gmail.com>
2013-05-14 15:22:48 -0700

    add a broken test for the force attribute

---
+++ fixtures/artifact_test/recipes/force_install.rb
@@ -0,0 +1,10 @@
+artifact_deploy "artifact_test" do
+  version node[:artifact_test][:version]
+  artifact_location "/tmp/kitchen-chef-solo/cookbooks/artifact/fixtures/artifact_test_force-1.2.3.tgz"
+  artifact_checksum node[:artifact_test][:checksum]
+  deploy_to node[:artifact_test][:deploy_to]
+  owner "artifacts"
+  group "artifact"
+
+  action :deploy
+end
