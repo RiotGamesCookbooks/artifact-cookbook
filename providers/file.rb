7723ccef816341fe547da9feaacbad5707cda275
Damien Raude-Morvan <drazzib@drazzib.com>
2017-03-15 22:02:48 +0100

    Upgrade nexus_cli gem to 4.1.1: Use latest httpclient 2.x gem

--- providers/file.rb
+++ providers/file.rb
@@ -32,7 +32,8 @@ def load_current_resource
   if Chef::Artifact.from_nexus?(new_resource.location)
 
     chef_gem "nexus_cli" do
-      version "4.0.2"
+      version "4.1.1"
+      action :upgrade
     end
 
     @nexus_configuration = new_resource.nexus_configuration
